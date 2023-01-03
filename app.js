e297cee6f0bcef980c47a765b26464f8ac67380c
naman114 <namangogia2001@gmail.com>
2023-01-03 22:32:40 +0530

    refactor: todos.ejs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { Todo } = require("./models");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -41,6 +42,7 @@ app.post("/todos", async function (request, response) {
   try {
     const todo = await Todo.addTodo(request.body);
     return response.json(todo);
+    // return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
